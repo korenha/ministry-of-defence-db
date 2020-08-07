a6722d086c1fec37b0b1088b319ab2dfbe4f833e
korenha <kh@sem-wolf.org>
2020-08-07 10:52:49 +0300

    remove redundant files

--- database_exercise/db/DBField.py
+++
@@ -1,8 +0,0 @@
-import db_api
-from typing import Type
-
-
-class DBField(db_api.DBField):
-    def __init__(self, name: str, type_: Type):
-        self.name = name
-        self.type = type_
--- database_exercise/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from .DataBase import DataBase
-from .DBTable import DBTable
-from .SelectionCriteria import SelectionCriteria
-from .DBField import DBField
